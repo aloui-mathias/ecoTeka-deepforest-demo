c8edb11f49a901c35c74114d6e06f4d0be443425
Mathias Aloui <mathias.aloui@gmail.com>
2021-07-20 18:53:33 +0200

    feat: prediction, environment.yml and example

--- script/functions.py
+++ script/functions.py
@@ -1,9 +1,14 @@
 import json
 import pyproj
 import numpy
+import pandas
+import cv2
 from urllib.parse import unquote, urlencode
 from owslib.wmts import WebMapTileService
 from PIL import Image
+from matplotlib import pyplot
+from shapely import geometry
+from deepforest import main
 from qgis.core import (
     QgsApplication,
     QgsProject,
@@ -14,12 +19,12 @@ from qgis.core import (
 )
 from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtCore import QSize, QEventLoop
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 
-def get_polygon(geojson_path: str) -> List:
+def get_polygon(geojson_path: str) -> List[List[float]]:
     try:
-        file = open(geojson_path, 'r')
+        file = open(geojson_path, 'r', encoding="utf-8")
         file.seek(0)
         geojson = json.loads(file.read())
     except:
@@ -97,7 +102,8 @@ def render_image(
         ymin: float,
         xmax: float,
         ymax: float,
-        path: str) -> None:
+        path: str,
+        high_resolution: bool) -> None:
 
     QGS = QgsApplication([], False)
     QGS.initQgis()
@@ -112,9 +118,13 @@ def render_image(
     settings = QgsMapSettings()
     settings.setLayers([WMTS_LAYER])
     settings.setBackgroundColor(QColor(255, 255, 255))
+    if high_resolution:
+        res = 0.2
+    else:
+        res = 0.25
     settings.setOutputSize(QSize(
-        int(extent.width() / 0.25),
-        int(extent.height() / 0.25)
+        int(extent.width() / res),
+        int(extent.height() / res)
     ))
     settings.setExtent(WMTS_LAYER.extent())
 
@@ -134,7 +144,127 @@ def render_image(
 
     QGS.exitQgis()
 
+    return
+
+
 def get_image(path: str) -> numpy.ndarray:
     image = Image.open(path, 'r')
     numpy_rgba = numpy.array(image).astype('float32')
     return numpy_rgba[:, :, :3]
+
+
+def convert_polygon(
+        polygon: List[List[float]],
+        image: numpy.ndarray,
+        xmin: float,
+        ymin: float,
+        xmax: float,
+        ymax: float) -> List:
+
+    polygon_image = []
+    width_ign = image.shape[1]
+    height_ign = image.shape[0]
+
+    for index in range(len(polygon)):
+        coord = polygon[index]
+        coord_ign = convert_coord(coord[0], coord[1], 4326, 3857)
+        point = ((coord_ign[0] - xmin)*(width_ign/(xmax-xmin)),
+                 (ymax - coord_ign[1])*(height_ign/(ymax-ymin)))
+        polygon_image.append(point)
+
+    return numpy.array(polygon_image).astype('int32')
+
+
+def predictions(
+        image: numpy.ndarray,
+        high_resolution: bool) -> pandas.DataFrame:
+
+    model = main.deepforest()
+    model.use_release()
+
+    if high_resolution:
+        patch_size = 800
+    else:
+        patch_size = 500
+
+    return model.predict_tile(
+        raster_path=None,
+        image=image,
+        patch_size=patch_size,
+        patch_overlap=0.15,
+        iou_threshold=0.15,
+        return_plot=False,
+        use_soft_nms=False,
+        sigma=0.5,
+        thresh=0.001)
+
+
+def draw_box(
+        image: numpy.ndarray,
+        box: List,
+        color: List,
+        thickness: float = 2) -> None:
+
+    b = numpy.array(box).astype(int)
+    cv2.rectangle(
+        img=image,
+        pt1=(b[0], b[1]),
+        pt2=(b[2], b[3]),
+        color=color,
+        thickness=thickness,
+        lineType=cv2.LINE_AA
+    )
+
+    return
+
+
+def draw_all_boxes(
+        image: numpy.ndarray,
+        boxes: pandas.DataFrame,
+        color: List = [0, 0, 255]) -> None:
+
+    for box in boxes[["xmin", "ymin", "xmax", "ymax"]].values:
+        draw_box(image, box, color)
+
+    return
+
+
+def save_image_predictions(
+        path: str,
+        image: numpy.ndarray,
+        predictions: pandas.DataFrame,
+        polygon: Optional[List[List[float]]] = None) -> None:
+
+    image_copy = image.copy().astype('uint8')
+
+    if polygon is None:
+        boxes = predictions
+    else:
+        zone = geometry.Polygon(polygon)
+        boxes = []
+        if predictions is not None:
+            for predicted_box in predictions.values:
+                coord = predicted_box[:4]
+                box_points = [
+                    [coord[0], coord[1]],
+                    [coord[0], coord[3]],
+                    [coord[2], coord[3]],
+                    [coord[2], coord[1]]
+                ]
+                box = geometry.Polygon(box_points)
+                intersection_area = box.intersection(zone).area
+                box_area = box.area
+                ioa = intersection_area / box_area
+                if ioa > 0.4:
+                    boxes.append(predicted_box)
+
+            boxes = pandas.DataFrame(boxes, columns=predictions.columns)
+
+        cv2.polylines(image_copy, [polygon], True, [255, 0, 0], thickness=10)
+
+    if boxes is not None:
+        draw_all_boxes(image_copy, boxes)
+
+    pyplot.imsave(path, image_copy)
+
+    return
--- script/main.py
+++ script/main.py
@@ -6,12 +6,21 @@ from functions import (
     convert_coord,
     get_ign_request,
     render_image,
-    get_image
+    get_image,
+    convert_polygon,
+    predictions,
+    save_image_predictions
 )
 
 
 parser = argparse.ArgumentParser()
 default_geojson = "data/export.geojson"
+parser.add_argument(
+    "--high-resolution",
+    help="can be used if 10 centimeters per pixel resolution"
+    + "is available.",
+    action="store_true"
+)
 parser.add_argument(
     "--geojson",
     help=(
@@ -26,16 +35,27 @@ parser.add_argument(
     "--tiff",
     help=(
         "use to change the path of the generated tiff file "
-        + ""
+        + "from the IGN"
         + f"(default: {default_tiff})"),
     type=str,
     default=default_tiff
 )
+default_png = "data/prediction.png"
+default_tiff = "data/image.tiff"
+parser.add_argument(
+    "--png",
+    help=(
+        "use to change the path of the generated png file "
+        + "with the detected trees and the polygon"
+        + f"(default: {default_png})"),
+    type=str,
+    default=default_png
+)
 args = parser.parse_args()
 
-polygon = get_polygon(args.geojson)
+osm_polygon = get_polygon(args.geojson)
 
-osm_coords = get_tile_coord_from_polygon(polygon)
+osm_coords = get_tile_coord_from_polygon(osm_polygon)
 
 xmin, ymin = convert_coord(osm_coords[0], osm_coords[1], 4326, 3857)
 xmax, ymax = convert_coord(osm_coords[2], osm_coords[3], 4326, 3857)
@@ -48,6 +68,24 @@ render_image(
     ymin,
     xmax,
     ymax,
-    args.tiff)
+    args.tiff,
+    args.high_resolution)
 
 image = get_image(args.tiff)
+
+image_polygon = convert_polygon(
+    osm_polygon,
+    image,
+    xmin,
+    ymin,
+    xmax,
+    ymax
+)
+
+image_predictions = predictions(image, args.high_resolution)
+
+save_image_predictions(
+    args.png,
+    image,
+    image_predictions,
+    image_polygon)
